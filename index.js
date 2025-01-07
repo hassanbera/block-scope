0398490c7dc965397939ea5fb3c911fdb5f310ed
Hasan Bera Kabadayı <hasanbera0619@gmail.com>
2025-01-07 22:19:12 +0300

    bitti

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 //Global scope
 
 const globalVariable = 'global var';
+let globalLet = 'global let';
 
 if(true){
     //Block scope
-    const blockVariable = 'block var';
-    console.log(blockVariable);
-    console.log(globalVariable);
+    const blockVar1=("ben block scope 1deyim");
+    let blockLet1=("ben block scope 1deyim");
+
+    console.log(blockVar1);
+    console.log(blockLet1);
+}
+
+if(true){
+    //Block scope 2
+    const blockVar2=("ben block scope 2deyim");
+    let blockLet2=("ben block scope 2deyim");
+    var blockVar3=("ben block scope tanımam");
+
+    console.log(blockVar2);
+    console.log(blockLet2);
 }
 
 console.log(globalVariable);
-console.log(blockVariable);
+console.log(globalLet);
+console.log(blockVar3);
